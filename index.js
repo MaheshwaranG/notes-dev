c9e9ce918d8f3521bf080989e3cf739019162fe7
Maheshwaran G <gmaheshwaranit@gmail.com>
2019-01-27 16:56:57 +0530

    till google signin option

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require("express");
 
 var app = express();
+const passport = require("passport");
+const googleStrategy = require("passport-google-oauth20");
+const keys = require("./config/keys");
+
+passport.use(
+  new googleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, callback) => {
+      console.log(accessToken);
+      console.log(profile);
+    }
+  )
+);
 
 app.get("/", (req, res) => {
   res.send({ welcome: "welcome to passport login" });
 });
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 
---
+++ info.js
@@ -0,0 +1,2 @@
+// Creating app... done, ⬢ ancient-retreat-92806
+// https://ancient-retreat-92806.herokuapp.com/ | https://git.heroku.com/ancient-retreat-92806.git
